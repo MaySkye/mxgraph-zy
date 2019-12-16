6083e9fc141fda7e8adcdeb8b10efcab5217a1c0
MaySkye <zhaoyihoho@163.com>
2019-12-16 11:57:06 +0800

    sitemap.html的有关操作

--- javascript/examples/grapheditor/www/sitemap/paintMap.js
+++ javascript/examples/grapheditor/www/sitemap/paintMap.js
@@ -1,6 +1,3 @@
-/*此js用于显示整个地图的情况*/
-let siteData;
-let siteLineData;
 let hashmap=new Map();
 let sta=1; //主要用来控制点击事件，1表示查看，2表示添加新的站点，3表示添加新的链路
 //以下4个变量，sta=3时使用
@@ -8,7 +5,6 @@ let startPoint; //起点
 let endPoint; //终点
 let flag=0; //0表示未选，1表示起点，2表示终点
 let temp_polyline; //存储新链路的对象
-
 function createXMLHttpRequest() {
     var xmlhttp = null;
     if (window.XMLHttpRequest) {
@@ -24,67 +20,38 @@ function getTime() {
     var date = new Date();
     return date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
 }
-function requestSiteInfo(map)
+function requestPaintInfo(map)
 {
     //请求数据服务  Access-Control-Allow-Origin:
-    let ip = "http://localhost";
-    let url = ip + ":8888/site/findall";
-    console.log("url:  " + url);
-    let xmlHttp = createXMLHttpRequest();
-    //xmlHttp.crossDomain="Access-Control-Allow-Origin";
-    if (xmlHttp == null) {
+    let paint_url =  "http://localhost:8888/siteline/findPaintInfo";
+    console.log("paint_url:  " + paint_url);
+    let paint_xmlHttp = createXMLHttpRequest();
+    if (paint_xmlHttp == null) {
         alert('not allow ajax');
         return;
     }
-    xmlHttp.open("GET", url, true);
-    xmlHttp.setRequestHeader('Access-Control-Allow-Origin', '*');
-    xmlHttp.setRequestHeader('Content-type', 'application/json');
-    xmlHttp.onreadystatechange = function () {
-        siteCallBack(map,xmlHttp);
-        //requestSiteLineInfo(map);
+    paint_xmlHttp.open("GET", paint_url, true);
+    paint_xmlHttp.setRequestHeader('Access-Control-Allow-Origin', '*');
+    paint_xmlHttp.setRequestHeader('Content-type', 'application/json');
+    paint_xmlHttp.onreadystatechange = function () {
+        PaintInfoCallBack(map,paint_xmlHttp);
     }; //call back when ready
-    xmlHttp.send();
+    paint_xmlHttp.send();
 }
-function requestSiteLineInfo(map)
-{
-    //请求数据服务  Access-Control-Allow-Origin:
-    let ip1 = "http://localhost";
-    let url1 = ip1 + ":8888/siteline/findall";
-    console.log("url1:  " + url1);
-    let xmlHttp1 = createXMLHttpRequest();
-    //xmlHttp.crossDomain="Access-Control-Allow-Origin";
-    if (xmlHttp1 == null) {
-        alert('not allow ajax');
-        return;
-    }
-    xmlHttp1.open("GET", url1, true);
-    xmlHttp1.setRequestHeader('Access-Control-Allow-Origin', '*');
-    xmlHttp1.setRequestHeader('Content-type', 'application/json');
-    xmlHttp1.onreadystatechange = function () {
-        siteLineCallBack(map,xmlHttp1)
-    }; //call back when ready
-    xmlHttp1.send();
-}
-
-function siteCallBack(map,xmlHttp) {
+function PaintInfoCallBack(map,xmlHttp) {
     if (xmlHttp.readyState == 4 && xmlHttp.status == 200) { // finish
         console.log("[" + getTime() + "]" + xmlHttp.responseText);
-        siteData = JSON.parse(xmlHttp.responseText);
+        let paintInfo = JSON.parse(xmlHttp.responseText);
+        let siteData=paintInfo[0]["siteInfo"];
+        let siteLineData=paintInfo[0]["siteLinkInfo"];
+        console.log("siteInfo:"+siteData);
+        console.log("siteLinkInfo:"+siteLineData);
         //可以改变一下标志
-        //createLevel(JSON.stringify(siteData),map, 1, 'red');
-        //createLevel(JSON.stringify(siteData),map, 2, 'blue');
-        //createLevel(JSON.stringify(siteData),map, 3, 'green');
-        return true;
-    }else{
-        return false;
-    }
-}
-function siteLineCallBack(map,xmlHttp1) {
-    if (xmlHttp1.readyState == 4 && xmlHttp1.status == 200) { // finish
-        console.log("[" + getTime() + "]" + xmlHttp1.responseText);
-        siteLineData= JSON.parse(xmlHttp1.responseText);
-        // 绘制线路
-        //createLine(JSON.stringify(siteData),JSON.stringify(siteLineData),map);
+        createLevel(siteData,map, 1, 'red');
+        createLevel(siteData,map, 2, 'blue');
+        createLevel(siteData,map, 3, 'green');
+        createLine(siteData,siteLineData,map);
+
         return true;
     }else{
         return false;
@@ -95,6 +62,7 @@ function siteLineCallBack(map,xmlHttp1) {
 function createLevel(siteData,map, level, color) {
     //处理siteData
     let jsonArray = JSON.parse(siteData);
+    console.log("jsonArray.length:"+jsonArray.length);
     for (let i = 0; i < jsonArray.length; i++) {
         if (jsonArray[i]["site_level"] == level) {
             let x = jsonArray[i]["site_localx"];
@@ -158,10 +126,10 @@ function addMarker(map, point, color, locationInfo) {
         },
     );
     marker.addEventListener('rightclick', (e) => {
-           if(flag==0){
-               flag=1;
-               alert("右击成功")
-           }
+            if(flag==0){
+                flag=1;
+                alert("右击成功")
+            }
         },
     );
     marker.addEventListener('dblclick', (e) => {
@@ -174,28 +142,28 @@ function addMarker(map, point, color, locationInfo) {
 
     // 鼠标移入时触发
     marker.addEventListener('mouseover', (e) => {
-          if(sta==3){
-               if(flag==1){
-                   temp_polyline = new BMap.Polyline([startPoint,marker.getPosition()], {strokeColor:"red",//设置颜色
-                       strokeWeight:3, //宽度
-                       strokeOpacity:0.5});//透明度
-                   map.addOverlay(temp_polyline);
-               }
-           }else{
-              map.openInfoWindow(opw, point); // 开启信息窗口
-          }
+            if(sta==3){
+                if(flag==1){
+                    temp_polyline = new BMap.Polyline([startPoint,marker.getPosition()], {strokeColor:"red",//设置颜色
+                        strokeWeight:3, //宽度
+                        strokeOpacity:0.5});//透明度
+                    map.addOverlay(temp_polyline);
+                }
+            }else{
+                map.openInfoWindow(opw, point); // 开启信息窗口
+            }
         },
     );
     // 鼠标移出时触发
     marker.addEventListener('mouseout', e => {
 
-        if(sta==3){
-            if(flag==1){
-                map.removeOverlay(temp_polyline);
+            if(sta==3){
+                if(flag==1){
+                    map.removeOverlay(temp_polyline);
+                }
+            }else{
+                //map.openInfoWindow(opw, point); // 开启信息窗口
             }
-        }else{
-            //map.openInfoWindow(opw, point); // 开启信息窗口
-        }
         },
     );
 }
@@ -238,57 +206,57 @@ function openInfo(content, point, map) {
 }
 // 创建连线
 function createLine(siteData,siteLineData,map) {
-        let siteArray = JSON.parse(siteData);
-        let lineArray = JSON.parse(siteLineData);
-        console.log("lineArray:"  + lineArray.length);
-        for(let a=0;a<siteArray.length;a++){
-            let xx = siteArray[a]["site_localx"];
-            let yy = siteArray[a]["site_localy"];
-            let temp_point=new BMap.Point(xx,yy);
-            hashmap.set(siteArray[a]["site_name"],temp_point);
-        }
-        for(let i=0;i<lineArray.length;i++){
+    let siteArray = JSON.parse(siteData);
+    let lineArray = JSON.parse(siteLineData);
+    console.log("lineArray:"  + lineArray.length);
+    for(let a=0;a<siteArray.length;a++){
+        let xx = siteArray[a]["site_localx"];
+        let yy = siteArray[a]["site_localy"];
+        let temp_point=new BMap.Point(xx,yy);
+        hashmap.set(siteArray[a]["site_name"],temp_point);
+    }
+    for(let i=0;i<lineArray.length;i++){
+        console.log("point1:"  + lineArray[i]["point1"]);
+        console.log("point2:"  + lineArray[i]["point2"]);
+        let point1=hashmap.get(lineArray[i]["point1"]);
+        let point2=hashmap.get(lineArray[i]["point2"]);
+        let line = [point1,point2];
+        addLine(map, line);
+        console.log("success" );
+    }
+    /*for (let i = 0; lineArray.length < 13; i++) {
+        //var point1;
+        //var point2;
+        if (lineArray[i]["point1"] != ""&&lineArray[i]["point2"] != "") {
             console.log("point1:"  + lineArray[i]["point1"]);
             console.log("point2:"  + lineArray[i]["point2"]);
-            let point1=hashmap.get(lineArray[i]["point1"]);
-            let point2=hashmap.get(lineArray[i]["point2"]);
-            let line = [point1,point2];
-            addLine(map, line);
-            console.log("success" );
-        }
-        /*for (let i = 0; lineArray.length < 13; i++) {
-            //var point1;
-            //var point2;
-            if (lineArray[i]["point1"] != ""&&lineArray[i]["point2"] != "") {
-                console.log("point1:"  + lineArray[i]["point1"]);
-                console.log("point2:"  + lineArray[i]["point2"]);
-                for(let j=0;j<siteArray.length;j++){
-                    if(siteArray[j]["site_name"]==lineArray[i]["point1"]){
-                        let x1 = siteArray[j]["site_localx"];
-                        let y1 = siteArray[j]["site_localy"];
-                        console.log("x1:"  + x1);
-                        console.log("y1:"  + y1);
-                        var point1 = new BMap.Point(x1,y1);
-                    }
-                    if(siteArray[j]["site_name"]==lineArray[i]["point2"]){
-                        let x2 = siteArray[j]["site_localx"];
-                        let y2 = siteArray[j]["site_localy"];
-                        console.log("x2:"  + x2);
-                        console.log("y2:"  + y2);
-                        var point2 = new BMap.Point(x2,y2);
-                    }
-                   /!* if(point1!=null&&point2!=null){
-                        break;
-                    }*!/
+            for(let j=0;j<siteArray.length;j++){
+                if(siteArray[j]["site_name"]==lineArray[i]["point1"]){
+                    let x1 = siteArray[j]["site_localx"];
+                    let y1 = siteArray[j]["site_localy"];
+                    console.log("x1:"  + x1);
+                    console.log("y1:"  + y1);
+                    var point1 = new BMap.Point(x1,y1);
                 }
+                if(siteArray[j]["site_name"]==lineArray[i]["point2"]){
+                    let x2 = siteArray[j]["site_localx"];
+                    let y2 = siteArray[j]["site_localy"];
+                    console.log("x2:"  + x2);
+                    console.log("y2:"  + y2);
+                    var point2 = new BMap.Point(x2,y2);
+                }
+               /!* if(point1!=null&&point2!=null){
+                    break;
+                }*!/
             }
-            //每一个画一个短线
-            let line = [point1,point2];
-            addLine(map, line);
-            //point1=null;
-            //point2=null;
-            //line=null;
-        }*/
+        }
+        //每一个画一个短线
+        let line = [point1,point2];
+        addLine(map, line);
+        //point1=null;
+        //point2=null;
+        //line=null;
+    }*/
 }
 // 创建连线
 function addLine(map, line) {
@@ -363,7 +331,3 @@ function add_site(map,status) {
 function draw_line(map,status) {
     sta=status;
 }
-/* document.ready */
-/*
-$(function () {
-});*/
